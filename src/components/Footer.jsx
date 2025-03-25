8645cb06f5c7cf73338ef57f08fceb499e2e2de6
Mandar Bavdane <mandarbavdane@gmail.com>
2025-03-25 15:45:50 +0530

    footer padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,7 +27,7 @@ const Footer = () => {
         <div className="bg-neutral-700 my-5 h-[1px] w-full" >
           <div className="flex md:flex-row flex-col md:items-center
           justify-between">
-            <p className="font-semibold text-gray text-xs">
+            <p className="font-semibold text-gray text-xs mt-2">
               Copyright @ 2024 Apple Inc. All rights reserved.
             </p>
 
